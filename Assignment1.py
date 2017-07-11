bd55d1b98172dc31093f57178b593dbec654849c
Darla Torres <torresdarla@gmail.com>
2017-07-11 03:40:48 +0000

    assignment 1.py

--- Assignment1.py
+++ Assignment1.py
@@ -14,10 +14,19 @@ yeast = raw_input()
 print "Enter the loaf adjustment factor (e.g. 2.0 doubles the size):",
 factor = raw_input()
 
-print "-- Modified Recipe --"
-print "BreadFlour: %0.2f cups." %(float(Flour)*int(factor))
-print "Water: %0.2f cups." %(float(water)*int(factor))
-print "Salt: %0.2f teaspoons." %(float(salt)*int(factor))
-print "Yeast: %0.2f teaspoons." %(float(yeast)*int(factor))
-print "Happy Baking!"
+print ""
 
+print "-- Modified Recipe --"
+print "BreadFlour: %0.2f cups." %(float(Flour)*float(factor))
+print "Water: %0.2f cups." %(float(water)*float(factor))
+print "Salt: %0.2f teaspoons." %(float(salt)*float(factor))
+print "Yeast: %0.2f teaspoons." %(float(yeast)*float(factor))
+print "Happy Baking!"
+20/4
+print ""
+print "-- Modified Recipe in Grams--"
+print "BreadFlour: %0.2f grams." %(float(Flour)*float(factor)*120.00)
+print "Water: %0.2f grams." %(float(water)*float(factor)*237.00)
+print "Salt: %0.2f grams." %(float(salt)*float(factor)*5.00)
+print "Yeast: %0.2f grams." %(float(yeast)*float(factor)*3.00)
+print "Happy Baking!"
