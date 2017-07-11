0aa2755316ebe239b0953eceff820a7735cefdb5
Darla Torres <torresdarla@gmail.com>
2017-07-11 03:50:16 +0000

    Assignment 1.py

--- Assignment1.py
+++ Assignment1.py
@@ -2,6 +2,7 @@
 #July 10, 2017
 #This program converts initial measurements of certain baking ingredients by a factor chosen by the use. The program then displays these converted amounts in both the original measurement as well as in grams. 
 
+#Program begins by asking user to input data about recipe
 print "-- Original Recipe --"
 
 print "Enter the amount of Flour (cups):",
@@ -16,6 +17,7 @@ print "Enter the loaf adjustment factor (e.g. 2.0 doubles the size):",
 factor = raw_input()
 
 print ""
+#Now the program will apply the factor to recipe ingredients
 
 print "-- Modified Recipe --"
 print "BreadFlour: %0.2f cups." %(float(Flour)*float(factor))
@@ -24,6 +26,7 @@ print "Salt: %0.2f teaspoons." %(float(salt)*float(factor))
 print "Yeast: %0.2f teaspoons." %(float(yeast)*float(factor))
 print "Happy Baking!"
 
+#Now the program will convert the previous measurements to grams
 print ""
 print "-- Modified Recipe in Grams--"
 print "BreadFlour: %0.2f grams." %(float(Flour)*float(factor)*120.00)
